96cbef245541be191f415e7662a4ca59146af210
karthic2914 <karthic2914@gmail.com>
2026-04-02 17:12:11 +0200

    add Vercel Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
 import App from './App';
 
 inject();
+injectSpeedInsights();
 
 createRoot(document.getElementById('root')).render(<App />);
