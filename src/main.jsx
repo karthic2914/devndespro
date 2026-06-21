f393b2299ba5cb6865e86323174b180f5c63e464
karthic2914 <karthic2914@gmail.com>
2026-06-21 13:24:53 +0200

    refactor: strip inline styles/scripts from template, load consolidated template.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { inject } from '@vercel/analytics';
 import { injectSpeedInsights } from '@vercel/speed-insights';
+import './template.css';
 
 import App from './App';
 
